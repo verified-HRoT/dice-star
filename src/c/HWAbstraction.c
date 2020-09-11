7c43aafef92fd099511eb7a0e23d71f962c6908e
Aseem Rastogi <aseemr@microsoft.com>
2020-09-11 13:32:16 +0530

    nit

--- src/c/HWAbstraction.c
+++ src/c/HWAbstraction.c
@@ -5,25 +5,24 @@ uint32_t uds_len = 32;
 HWState_state st ()
 {
   HWState_l0_image_t img = { 1U, (uint8_t *) 1,
-		     (uint8_t *) 1, 1U, (uint8_t *) 1,        
-	(uint8_t *) 1, (uint8_t *) 1 };
+			     (uint8_t *) 1, 1U, (uint8_t *) 1,        
+			     (uint8_t *) 1, (uint8_t *) 1 };
   HWState_state sta = { (uint8_t *) 1, img };
   return sta;
 }
 
-  void read_uds(uint8_t *uds) {
-			       return;
+void read_uds(uint8_t *uds) {
+  return;
 }
 
-  void platform_zeroize_stack () {
-				  return;
-  }
+void platform_zeroize_stack () {
+  return;
+}
 
 void disable_uds () {
-		     return;
+  return;
 }
 
 void platform_zeroize (uint32_t len, uint8_t* buf) {
-						    return;
+  return;
 }
-
