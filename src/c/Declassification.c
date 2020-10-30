81825bd579efe0a48d0c825a4cf620fee1b08436
Aseem Rastogi <aseemr@microsoft.com>
2020-10-30 13:44:46 +0530

    no prefixes

--- src/c/Declassification.c
+++ src/c/Declassification.c
@@ -2,12 +2,12 @@
 
 #include "RIoT_Declassify.h"
 
-void RIoT_Declassify_declassify_secret_buffer (uint32_t len, uint8_t *src, uint8_t *dst)
+void declassify_secret_buffer (uint32_t len, uint8_t *src, uint8_t *dst)
 {
   memcpy (dst, src, len);
 }
 
-void RIoT_Declassify_classify_public_buffer (uint32_t len, uint8_t *src, uint8_t *dst)
+void classify_public_buffer (uint32_t len, uint8_t *src, uint8_t *dst)
 {
   memcpy (dst, src, len);
 }
